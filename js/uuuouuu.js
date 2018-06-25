79de7ac272749a62d6c65a4998d870816e34e976
Skorobogatov Kirill <38665816+Galiactus@users.noreply.github.com>
2018-06-25 15:47:07 +0300

    Add files via upload

---
+++ js/uuuouuu.js
@@ -0,0 +1,125 @@
+$(function reew(){
+
+  $("#meu_div0").click(function(){
+    $("#qqq0").toggleClass("spinEffect");
+  });
+});
+
+
+$(function rew(){
+
+  $("#meu_div").click(function(){
+    $("#qqq").toggleClass("spinEffect");
+  });
+});
+
+$(function werty(){
+
+
+  $("#meu_div2").click(function(){
+    $("#qqq2").toggleClass("spinEffect");
+  });
+
+});
+
+
+$(function poyut(){
+
+
+  $("#meu_div3").click(function(){
+    $("#qqq3").toggleClass("spinEffect");
+  });
+});
+
+$(function poyuto(){
+
+
+  $("#meu_div4").click(function(){
+    $("#qqq4").toggleClass("spinEffect");
+  });
+});
+
+$(function poyqwuto(){
+
+
+  $("#meu_div5").click(function(){
+    $("#qqq5").toggleClass("spinEffect");
+  });
+});
+
+$(function poypputo(){
+
+
+  $("#meu_div6").click(function(){
+    $("#qqq6").toggleClass("spinEffect");
+  });
+});
+$(function pouuyuto(){
+
+
+  $("#meu_div7").click(function(){
+    $("#qqq7").toggleClass("spinEffect");
+  });
+});
+$(function poyullto(){
+
+
+  $("#meu_div8").click(function(){
+    $("#qqq8").toggleClass("spinEffect");
+  });
+});
+$(function poaazyuto(){
+
+
+  $("#meu_div9").click(function(){
+    $("#qqq9").toggleClass("spinEffect");
+  });
+});
+$(function pozzxyuto(){
+
+
+  $("#meu_div10").click(function(){
+    $("#qqq10").toggleClass("spinEffect");
+  });
+});
+$(function poyllkjhguto(){
+
+
+  $("#meu_div11").click(function(){
+    $("#qqq11").toggleClass("spinEffect");
+  });
+});
+
+$(function poyilnvkjhguto(){
+
+  $("#meu_div12").click(function(){
+    $("#qqq12").toggleClass("spinEffect");
+  });
+});
+
+
+$(function pzzzaato(){
+
+  $("#meu_div13").click(function(){
+    $("#qqq13").toggleClass("spinEffect");
+  });
+});
+
+$(function pzpzpzpzpzp(){
+
+  $("#meu_div14").click(function(){
+    $("#qqq14").toggleClass("spinEffect");
+  });
+});
+
+$(function zyzyzyyzzy(){
+
+  $("#meu_div15").click(function(){
+    $("#qqq15").toggleClass("spinEffect");
+  });
+});
+
+
+$("h1").slabText({
+            "viewportBreakpoint":380
+});
